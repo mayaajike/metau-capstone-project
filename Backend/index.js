4eb019af67b80b2421f4ca0a13cd1d7c2276d3c6
Maya Ody-Ajike <mayaajike@meta.com>
2024-06-26 18:35:18 -0700

    new change

--- Backend/index.js
+++ Backend/index.js
@@ -21,7 +21,6 @@ app.get('/', (req, res) => {
     res.send('Welcome to my app!')
 })
 
-
 const hashPassword = async (password) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
